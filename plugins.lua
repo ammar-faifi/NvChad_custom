cc8c240578f72a3e649ceba4f7fbdf9ef6b20529
ammar-faifi <me@ammarf.sa>
2023-12-09 11:18:07 +0300

    add surround.nvim

--- plugins.lua
+++ plugins.lua
@@ -1,13 +1,22 @@
 local plugins = {
+  -- esailly surround text with parenth
   {
-      'glacambre/firenvim',
-
-      -- Lazy load firenvim
-      -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-      lazy = not vim.g.started_by_firenvim,
-      build = function()
-          vim.fn["firenvim#install"](0)
-      end
+    'kylechui/nvim-surround',
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    'glacambre/firenvim',
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    lazy = not vim.g.started_by_firenvim,
+    build = function()
+        vim.fn["firenvim#install"](0)
+    end
   },
 
   {
