901a9bbce59bab90c6d282673bcca93ef758bba0
ammar-faifi <me@ammarf.sa>
2024-02-07 13:15:46 +0300

    fix lsp for C

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,6 +4,9 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local servers = { "html", "pyright", "tsserver", "texlab", "clangd" }
 
+-- I needed this encoding for `clangd`
+capabilities.offsetEncoding = { "utf-16" }
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
